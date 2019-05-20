b6d274b5a2c9b9b02fd2488d374633e040bb78ac
Julian Piron <pironj27@gmail.com>
2019-05-20 15:22:19 -0700

    added friend.js data including script for survey.html

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends who complete the survey.
+// Initially the array will contain tests until more users take teh survey once deployed.
+// ===============================================================================
+
+var friendsArr = [
+  {
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+]
+
+module.exports = friendsArr;
