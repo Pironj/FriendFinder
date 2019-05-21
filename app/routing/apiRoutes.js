441a8d3b81f20ad03d2c500c236c67e104758012
Julian Piron <pironj27@gmail.com>
2019-05-21 11:58:16 -0700

    absulte difference added.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,22 +10,42 @@ module.exports = function(app) {
   });
   
   app.post("/api/friends", function(req, res) {
-    friendsData.push(req.body);
-
-
+    
+    
     var parseData = req.body;
-
+    var bestFriendArr = [];
+    
     for (i = 0; i < parseData.scores.length; i++) {
       parseData.scores[i] = parseInt(parseData.scores[i]);
     }
-    console.log(parseData);
+    // logs the current survey entry scores.
+    console.log(parseData.scores);
+    for (i = 0; i < friendsData.length; i++) {
+      // logs each friend in our api.
+      console.log(friendsData[i].scores);
+      var totalDifference = 0;
+      for (z = 0; z < friendsData[i].scores.length; z++) {
+        console.log(friendsData[i].scores[z])
+        var absoluteDifference = Math.abs(parseData.scores[z] - friendsData[i].scores[z]);
+        // increments the total abs
+        totalDifference += absoluteDifference;
+      }
+      console.log("----------------");
+      console.log(totalDifference)
+      bestFriendArr.push(totalDifference);
+      console.log(bestFriendArr);
+      var match = Math.min.apply(null, bestFriendArr);
+      console.log(match);
+      // var friendMatch = friendsData[i]
+    }
+    // parseData.scores.forEach(element => {
+    //   if (bestFriend < totalDifference) {
+    //     console.log(bestFriend);
+    //   }
+      
+    // });
+    friendsData.push(req.body);
   });
-
+  
 }
 
-for (i = 0; i < friendsData.length; i++) {
-  var totalDifference = 0;
-  for (z = 0; z < friendsData[i].scores.length; z++) {
-    var absoluteDiff = Math.abs()
-  }
-}
